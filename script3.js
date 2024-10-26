0463133d08ef3d56520cf9153329daf764e3887b
maritsukiii <155792045+maritsukiii@users.noreply.github.com>
2024-10-26 22:45:05 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,27 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const audioPlayer = document.getElementById('audioPlayer');
-    const lyricsContainer = document.getElementById('lyricsContainer');
-    const lyricsLines = lyricsContainer.getElementsByTagName('p');
-  
-    function updateLyrics() {
-      const currentTime = audioPlayer.currentTime;
-  
-      for (let i = 0; i < lyricsLines.length; i++) {
-        const lineTime = parseFloat(lyricsLines[i].getAttribute('data-time'));
-        const nextLineTime = lyricsLines[i + 1] ? parseFloat(lyricsLines[i + 1].getAttribute('data-time')) : Infinity;
-  
-        if (currentTime >= lineTime && currentTime < nextLineTime) {
-          lyricsLines[i].classList.add('active');
-          lyricsLines[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
-        } else {
-          lyricsLines[i].classList.remove('active');
-        }
-      }
-    }
-  
-    audioPlayer.addEventListener('timeupdate', updateLyrics);
-  });
+            const audioPlayer = document.getElementById('audioPlayer');
+            const lyricsContainer = document.getElementById('lyricsContainer');
+            const lyricsLines = lyricsContainer.getElementsByTagName('p');
+            let lastActiveIndex = -1;
 
+            function updateLyrics() {
+                const currentTime = audioPlayer.currentTime;
+
+                for (let i = 0; i < lyricsLines.length; i++) {
+                    const lineTime = parseFloat(lyricsLines[i].getAttribute('data-time'));
+                    const nextLineTime = lyricsLines[i + 1] ? parseFloat(lyricsLines[i + 1].getAttribute('data-time')) : Infinity;
+
+                    if (currentTime >= lineTime && currentTime < nextLineTime) {
+                        if (lastActiveIndex !== i) {
+                            if (lastActiveIndex !== -1) {
+                                lyricsLines[lastActiveIndex].classList.remove('active');
+                                lyricsLines[lastActiveIndex].style.display = 'none';
+                            }
+                            lyricsLines[i].classList.add('active');
+                            lyricsLines[i].style.display = 'block';
+                            lyricsLines[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
+                            lastActiveIndex = i;
+                        }
+                    }
+                }
+            }
+
+            audioPlayer.addEventListener('timeupdate', updateLyrics);
+        });
    // Получаем элемент уведомления
    const notification = document.getElementById('fullscreen-notification');
 
